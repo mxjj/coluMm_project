65c81621be83e2ea74acc1682aa37ef068e41d63
m2018ming@163.com <m2018ming@163.com>
2020-02-19 19:59:44 +0800

    '完成样式'

--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -29,8 +29,47 @@
         </div>
       </div>
       <div class="nav_videos">
-        <div class="vidos_i">
-          
+        <div
+          class="vidos_i"
+          v-for="(item,index) in bag_Img"
+          :key="index"
+          :style="{'background-Image':'url('+item.bg_Image+')'}"
+        ></div>
+      </div>
+    </div>
+    <div class="floor_box padd-30">
+      <div class="fl_title">
+        <div class="fl_i_title">
+          <div class="fs-28b mb-14 col-333">精品推荐</div>
+          <div class="fs-20 col-999">诚意推荐品质商品</div>
+        </div>
+        <div class="fr-i_nav fs-24 col-999">更多</div>
+      </div>
+      <!-- 轮播图 -->
+      <div class="floor_Image">
+        <Swipe :autoplay="3000">
+          <SwipeItem v-for="(image, index) in images" :key="index">
+            <img v-lazy="image" />
+          </SwipeItem>
+        </Swipe>
+      </div>
+      <!-- 商品列表 -->
+      <div class="floor_nav">
+        <div class="fl_nav_item">
+          <div class="nav_img">
+            <img src />
+          </div>
+          <div class="nav_text">
+            <div class="nav_tit fs-32 col-999">每晚微量天然蚕丝春秋夏被单双...</div>
+            <div class="nav_price_t">
+              <div class="fs-36b col-F31">￥1.00</div>
+            </div>
+            <div class="nav_pri_b">
+              <div class="fs-28b col-000">￥0.99</div>
+              <div class="fs-18b vip_box">VIP</div>
+              <div class="fs-24 col-999">已售888件</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -50,6 +89,14 @@ export default {
       images: [
         "https://img.yzcdn.cn/vant/apple-1.jpg",
         "https://img.yzcdn.cn/vant/apple-2.jpg"
+      ],
+      bag_Img: [
+        {
+          bg_Image: require("../../assets/home_Img/school.png")
+        },
+        {
+          bg_Image: require("../../assets/home_Img/vidoes.png")
+        }
       ],
       icon_list: [
         {
@@ -81,7 +128,7 @@ export default {
       .header_search {
         margin: 0 auto;
         height: 64px;
-        width: 306px;
+        width: 360px;
         display: flex;
         justify-content: center;
         align-items: center;
@@ -125,10 +172,92 @@ export default {
       }
     }
     .nav_videos {
-     padding: 30px;
-     background-color: #F6F6F6;
-     border: 1px solid red;
+      padding: 30px;
+      display: flex;
+      justify-content: space-between;
+      background-color: #f6f6f6;
+      .vidos_i {
+        height: 170px;
+        background-size: 100% 100%;
+        background-repeat: no-repeat;
+        width: 48%;
+      }
     }
+  }
+  .floor_box {
+    .fl_title {
+      display: flex;
+      .fl_i_title {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        height: 120px;
+      }
+      .fr-i_nav {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+      }
+    }
+    .floor_Image {
+      position: relative;
+      height: 300px;
+      img {
+        height: 260px;
+        width: 100%;
+      }
+    }
+    .floor_nav {
+      .fl_nav_item {
+        padding: 30px 0;
+        height: 240px;
+        box-sizing: border-box;
+        display: flex;
+        justify-content: space-between;
+        .nav_img {
+          height: 180px;
+          width: 180px;
+          img {
+            height: 180px;
+            width: 180px;
+          }
+        }
+        .nav_text {
+          .nav_tit {
+            margin-bottom: 54px;
+          }
+          .nav_price_t{
+            margin-left: -6px;
+          }
+          .nav_pri_b {
+            display: flex;
+            align-items: center;
+            margin-top: 16px;
+            .vip_box {
+              margin: 0 10px;
+              height: 20px;
+              line-height: 20px;
+              text-align: center;
+              width: 56px;
+              background-color: #000;
+              color: #d1b02c;
+            }
+          }
+        }
+      }
+    }
+  }
+  .van-swipe__indicator {
+    height: 8px;
+    width: 14px;
+    margin: 0 5px;
+    background: #fd8e78;
+  }
+  .van-swipe__indicator--active {
+    width: 24px;
+    background-color: #fe3f31;
   }
 }
 </style>
